98fa22c59f31537358cd4afeeaaa1a49ece7b82f
kimtaejun <kim_taejun@naver.com>
2021-08-06 18:56:35 +0900

    로그인 유지

--- src/main/java/com/attendance/infra/config/SecurityConfig.java
+++ src/main/java/com/attendance/infra/config/SecurityConfig.java
@@ -3,17 +3,27 @@ package com.attendance.infra.config;
 import com.attendance.modules.account.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
 
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
+    private final DataSource dataSource;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
@@ -27,11 +37,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/admin-page", "/admin/place/**").hasRole(Role.ADMIN.name())
                 .anyRequest().authenticated();
 
-
         http.formLogin()
                 .loginPage("/login").permitAll();
 
         http.logout().logoutSuccessUrl("/");
+
+        http.rememberMe()
+                .userDetailsService(userDetailsService)
+                .tokenRepository(tokenRepository());
+    }
+
+    private PersistentTokenRepository tokenRepository() {
+        JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
+        tokenRepository.setDataSource(dataSource);
+
+        return tokenRepository;
     }
 
     @Override
---
+++ src/main/java/com/attendance/modules/account/PersistentLogins.java
@@ -0,0 +1,34 @@
+package com.attendance.modules.account;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "persistent_logins")
+public class PersistentLogins {
+
+
+    @Id
+    @Column(length = 64)
+    private String series;
+
+    @Column(length = 64, nullable = false)
+    private String username;
+
+    @Column(length = 64, nullable = false)
+    private String token;
+
+    @Column(nullable = false, name = "last_used")
+    private LocalDateTime lastUsed;
+
+
+
+}
